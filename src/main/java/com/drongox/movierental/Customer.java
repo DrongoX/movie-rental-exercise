6d09bbf4733eeddfd728455080691c23b97d812c
Ilya Zinchuk <31203998+DrongoX@users.noreply.github.com>
2019-02-26 11:44:12 +0100

    Update Customer.java

--- src/main/java/com/drongox/movierental/Customer.java
+++ src/main/java/com/drongox/movierental/Customer.java
@@ -45,7 +45,7 @@ public class Customer {
   }
 
 
-  public class Movie {
+  public static class Movie {
     public static final int  CHILDRENS = 2;
     public static final int  REGULAR = 0;
     public static final int  NEW_RELEASE = 1;
