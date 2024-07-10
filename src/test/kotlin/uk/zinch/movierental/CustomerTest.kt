6cc463f7276eb06aaeaeb4dc0dd356067b6147f5
moustapha SIDI <moustapha.sidi-el-messioub.ext@veolia.com>
2024-07-10 15:29:59 +0200

    add tests

---
+++ src/test/kotlin/uk/zinch/movierental/CustomerTest.kt
@@ -0,0 +1,49 @@
+package uk.zinch.movierental;
+
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+import uk.zinch.movierental.Customer.Movie
+import uk.zinch.movierental.Customer.Tape
+
+class CustomerTest: DescribeSpec({
+    it("should print statement without rentals"){
+
+        val customer = Customer("El moatamid")
+        val result = customer.statement()
+
+        result shouldBe "Rental Record for El moatamid\n" +
+                "Amount owed is 0.0\n" +
+                "You earned 0 frequent renter points"
+    }
+
+    it("should print statement for renting a children movie for one day"){
+        val customer = Customer("El moatamid")
+        val movie = Movie("Tom & jerry", 2)
+        val tape = Tape("Serail_Number", movie)
+        val rental = Customer.Rental(tape, 1)
+        customer.addRental(rental)
+
+        val result = customer.statement()
+
+        result shouldBe "Rental Record for El moatamid\n" +
+                "\tTom & jerry\t1.5\n" +
+                "Amount owed is 1.5\n" +
+                "You earned 1 frequent renter points"
+    }
+
+
+    it("should print statement for renting a children movie for four days"){
+        val customer = Customer("El moatamid")
+        val movie = Movie("Tom & jerry", 2)
+        val tape = Tape("Serail_Number", movie)
+        val rental = Customer.Rental(tape, 4)
+        customer.addRental(rental)
+
+        val result = customer.statement()
+
+        result shouldBe "Rental Record for El moatamid\n" +
+                "\tTom & jerry\t3.0\n" +
+                "Amount owed is 3.0\n" +
+                "You earned 1 frequent renter points"
+    }
+})
